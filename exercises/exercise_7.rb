d8d4c2105fd0fdbfaa941c0a9d448a8d1d1f848a
Justin <justinjdbennett@gmail.com>
2023-01-23 22:32:43 -0700

    Complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -6,7 +6,19 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
-puts "Exercise 7"
-puts "----------"
+puts 'Exercise 7'
+puts '----------'
 
 # Your code goes here ...
+
+puts 'Please enter a store name'
+print '> '
+
+given_name = gets.chomp
+
+new_store = Store.create(name: given_name)
+
+puts '--↓ Error creating store ↓--' if new_store.errors.any?
+puts new_store.errors.full_messages
+
+puts '----------'
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  validate :must_have_apparel
+
+  def must_have_apparel
+    return unless !mens_apparel && !womens_apparel
+
+    errors.add(:name, 'must carry some type of apparel')
+  end
 end
